bceea6382daed85e7c328da568b38f98259d777a
Dominik Płatos <dominik.platos@gmail.com>
2017-05-18 19:43:47 +0200

    Komunikacja js<->html

--- www/js/stockMarket.js
+++ www/js/stockMarket.js
@@ -1,5 +1,11 @@
 function start(){
-var url = "http://mybank.pl/gielda/indeks-wig20.html";
+document.addEventListener("deviceready",onDeviceReady, false);
+}
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+function getIndecesValue(){
+var url = "http://mybank.pl/gielda/"+document.querySelector('.stockName').value+".html";
     $.ajax({
             type: "GET",
             url: url,
@@ -9,6 +15,7 @@ var url = "http://mybank.pl/gielda/indeks-wig20.html";
             data=$(data).find('.g_tab:nth-child(1) tr:nth-child(3) td b:nth-child(1)').text()
               console.log(data)
               console.log(typeof data)
+              document.querySelector(".stockInformation a#stockIndex").innerHTML = data;
             }
             ,
             error: function(xhr, textStatus, errorThrown) {
