792b88e912c12e49e3ad00d58c69bd5a1246b6ad
OlaStadnik93 <olastadnik93@gmail.com>
2017-05-19 22:24:16 +0200

    Dodanie kontroli przyspieszenia

--- www/js/stockMarket.js
+++ www/js/stockMarket.js
@@ -1,5 +1,22 @@
+function onSuccess(acceleration) {
+//    alert('Acceleration X: ' + acceleration.x + '\n' +
+//          'Acceleration Y: ' + acceleration.y + '\n' +
+//          'Acceleration Z: ' + acceleration.z + '\n' +
+//          'Timestamp: '      + acceleration.timestamp + '\n');
+if(acceleration.z<0)
+getIndecesValue()
+}
+
+function onError() {
+    alert('onError!');
+}
+
+var options = { frequency: 300 };  // Update every 0.3 seconds
+
+
 function start(){
-document.addEventListener("deviceready",onDeviceReady, false);
+document.addEventListener("deviceready",onSuccess, false);
+//var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options); //Wykomentowany poniewaz browser sobie z tym nie radzi
 }
 function onDeviceReady() {
 	navigator.notification.beep(1);
