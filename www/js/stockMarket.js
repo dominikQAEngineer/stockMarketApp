1acdb3842c10394d8976eead8185854f18509ffd
Dominik Płatos <dominik.platos@gmail.com>
2017-05-21 15:02:39 +0200

    Dynamiczne tworzenie listy firm

--- www/js/stockMarket.js
+++ www/js/stockMarket.js
@@ -1,10 +1,10 @@
 var options = { frequency: 300 };  // Update every 0.3 seconds
-
+var htmlOfPage = "";
 //Function responsible for refresh current stock index after movement of device (not working on browser
 function refreshMovement(acceleration) {
 var deviceManufacturer  = device.manufacturer;
 if(acceleration.z<0 && deviceManufacturer!="unknown")
-getIndecesValue()
+getAllIndeces()
 }
 function onError() {
     alert('onError!');
@@ -13,45 +13,50 @@ function start(){
 document.addEventListener("deviceready",onDeviceReady, false);
 }
 function onDeviceReady() {
-//	navigator.notification.beep(1);
+    loadCompanyList()
 	var watchID = navigator.accelerometer.watchAcceleration(refreshMovement, onError, options);
 }
-function getIndecesValue(){
-var url = "http://mybank.pl/gielda/"+document.querySelector('.stockName').value+".html";//Ustawienie url strony glownej
-    $.ajax({
-            type: "GET",//co ma funkcja robic - pobierac cos ze strony
-            url: url, //adres strony podany powyzej
-            dataType: "html", //pobiera kod html
-            success: function (data) { //jezeli polaczyl sie z witryna, to do zmiennej "data" wrzuca to co pobral
-            $("#status").val(JSON.stringify(data)); //sprawdzamy status tego co pobral
-            data=$(data).find('.g_tab:nth-child(1) tr:nth-child(3) td b:nth-child(1)').text()//wyciagamy z tego co pobral wartosc indeksu
-              console.log(data)
-              document.querySelector(".stockInformation a#stockIndex").innerHTML = data;//wrzucamy do pliku html w miejsce selectora dana wartosc indeksu
-            }
-            ,
-            error: function(xhr, textStatus, errorThrown) {//jakies tam informacje o ewentualnym bledzie
-                    $("#status").val("Unavailable: " + textStatus);
-                }
-          });
-}
-function getAllIndeces(){
+function loadCompanyList(){
 var url = "http://mybank.pl/gielda/indeks-mwig40.html";
-var company ={companyName: "", measureTime: "", curIndex: "", prevIndex: "", percentChange: "", pointChange: "", curAssets: ""};
-    var htmlOfPage =
+var $companyList = $('#companyListSelect');
+    htmlOfPage =
     $.ajax({
                 async:false,
                 type: "GET",
                 url: url,
                 dataType: "html",
-//                success: function (data) {
-//                $("#status").val(JSON.stringify(data));
-//                data=$(data).find(".g_tab:nth-of-type(3)")
-//                },
-//                error: function(xhr, textStatus, errorThrown) {
-//                        $("#status").val("Unavailable: " + textStatus);
-//                    }
+                success: function (data) {
+                        document.querySelector(".stockInformation a#wig40StockIndex").innerHTML =
+                        $(data).find('.g_tab:nth-child(1) tr:nth-child(3) td b:nth-child(1)').text();
+              }
               }).responseText;
+              $companyList.empty();
+              $companyList.append('<option value="-1">Wybierz spolke</option>');
               htmlOfPage=$(htmlOfPage).find(".g_tab:nth-of-type(3)")
+for(var i=3;i<=42;i++){
+            $companyList.append('<option value="'+
+            i+
+            '">'+$(htmlOfPage).find("tr:nth-of-type("+i+") td:nth-of-type(1) b").text()+'</option>');
+            }
+}
+function getAllIndeces(){
+var url = "http://mybank.pl/gielda/indeks-mwig40.html";
+var company ={companyName: "", measureTime: "", curIndex: "", prevIndex: "", percentChange: "", pointChange: "", curAssets: ""};
+//    htmlOfPage =
+//    $.ajax({
+//                async:false,
+//                type: "GET",
+//                url: url,
+//                dataType: "html",
+//                success: function (data) {
+//              document.querySelector(".stockInformation a#wig40StockIndex").innerHTML =
+//              $(data).find('.g_tab:nth-child(1) tr:nth-child(3) td b:nth-child(1)').text();
+//                }
+////                error: function(xhr, textStatus, errorThrown) {
+////                        $("#status").val("Unavailable: " + textStatus);
+////                    }
+//              }).responseText;
+//              htmlOfPage=$(htmlOfPage).find(".g_tab:nth-of-type(3)")
 for(var i=3;i<=42;i++){
             company.companyName=  $(htmlOfPage).find("tr:nth-of-type("+i+") td:nth-of-type(1) b").text()
             company.measureTime=  $(htmlOfPage).find("tr:nth-of-type("+i+") td:nth-of-type(2)").text()
@@ -64,4 +69,9 @@ for(var i=3;i<=42;i++){
              console.log("Nazwa spolki: %s, Czas pomiaru: %s, Aktualny Indeks %s, Poprzedni Indeks %s, Zmiana procentowa %s, Zmiana punktowa %s, Obrot %s",
              company.companyName, company.measureTime, company.curIndex, company.prevIndex, company.percentChange, company.pointChange, company.curAssets);
             }
+}
+function getSelectedCompanyInformation(){
+//    if( $("#companyListSelect option:selected").value()!=-1)
+//    document.querySelector(".selectedCompanyIndexInfo a#currentIndexValue").innerHTML =
+//    $(htmlOfPage).find("tr:nth-of-type("+$("#companyListSelect option:selected").value()+") td:nth-of-type(3) b").text()
 }
