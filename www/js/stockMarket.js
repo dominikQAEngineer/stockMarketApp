be33bf7eecb18c10865ab0f3eeda9a54c9f16967
OlaStadnik93 <olastadnik93@gmail.com>
2017-08-20 13:46:54 +0200

    usuniecie konfliktow

--- www/js/stockMarket.js
+++ www/js/stockMarket.js
@@ -55,11 +55,11 @@ $companyList = $('#companyListSelect2');
                 }
 
     window.localStorage.setItem("listOfCompanyStorage", JSON.stringify(listOfCompanyStorage));
-    // var retrieveInfo = window.localStorage.getItem("listOfCompanyStorage");
-    // var parsedInfo = JSON.parse(retrieveInfo);
-    // for(var i=0;i<40;i++){
-    //         $companyList.append('<option value="'+(i+3)+'">'+parsedInfo[i]+'</option>');
-    //         }
+    var retrieveInfo = window.localStorage.getItem("listOfCompanyStorage");
+    var parsedInfo = JSON.parse(retrieveInfo);
+    for(var i=0;i<40;i++){
+            $companyList.append('<option value="'+(i+3)+'">'+parsedInfo[i]+'</option>');
+            }
 //// Without localStorage:
 //    for(var i=3;i<=42;i++){
 //            $companyList.append('<option value="'+
